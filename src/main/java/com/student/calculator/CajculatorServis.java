d13c510af89e41fd34c0eec2f18c1ad3fd959612
Аня <grigorevna.antoninka@mail.ru>
2023-03-19 16:35:00 +0700

    домашняя работа Spring Boot

---
+++ src/main/java/com/student/calculator/CajculatorServis.java
@@ -0,0 +1,8 @@
+package com.student.calculator;
+
+public interface CajculatorServis {
+    public int sum( int num1,int num2);
+    public int minus( int num1,int num2);
+    public int umnogit( int num1,int num2);
+    public int podelit( int num1,int num2);
+}
